ea62eae0a91686b626fa6e041f0942df186bfe12
psteo <teopersay@hotmail.com>
2020-10-26 12:08:31 +0800

    a

---
+++ tst.js
@@ -0,0 +1,16 @@
+mywd={
+    "Sheet1": [
+        {
+            "Phrases": "神透过耶稣基督的生"
+        },
+        {
+            "Phrases": "Home church is a great accomplishment."
+        },
+        {
+            "Phrases": "Through the home church destiny you will go to heaven"
+        },
+        {
+            "Phrases": "I worked my whole life to complete the home church foundation."
+        },
+     ]
+}
